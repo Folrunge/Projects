563cf403311741ffdf4fe30f2f451c6b66725bcd
Folrunge <folrunge@gmail.com>
2018-08-19 11:42:17 +0200

    Grid system added. Responsive for media min 400 done

--- Portfolio/front.js
+++ Portfolio/front.js
@@ -1,27 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-	load_page('Home');
-	document.querySelectorAll('.nav-link').forEach(link => {
-		link.onclick = () => {
-			load_page(link.dataset.page);
-			return false;
-		};
-	});
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
 });
-
-function load_page(name) {
-	const request = new XMLHttpRequest();
-	request.open('GET', `/${name}`);
-	request.onload = () => {
-		const response = request.responseText;
-		document.querySelector('#top').innerHTML = response;
-	};
-	request.send();
-}
-
-/*
-$(document).ready(function() {
-	$("#Home").prepend('<i class="fa fa-home fa-2x"></i>');
-	$("#About").prepend('<i class="fa fa-user fa-2x"></i>');
-	$("#Projects").prepend('<i class="fa fa-code fa-2x"></i>');
-	$("#Hobby").prepend('<i class="fa fa-gamepad fa-2x" style></i>');
-}) */
